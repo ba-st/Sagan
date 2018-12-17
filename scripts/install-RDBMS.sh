0bc41480904036e77ee8326c57594b706c7f7b2e
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2018-12-17 08:56:58 -0300

    Fixed shebang

--- scripts/install-RDBMS.sh
+++ scripts/install-RDBMS.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -1,4 +1,4 @@
-#!/bin/env bash
+#!/usr/bin/env bash
 
 psql -c "ALTER USER postgres PASSWORD 'secret'" -U postgres;
 psql -c "CREATE DATABASE test" -U postgres;
