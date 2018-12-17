b9b92fa3524be77c1f45ec3aaf2093cafb6b3484
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2018-12-17 19:24:01 -0300

    Add mariaDB to build matrix

--- scripts/install-RDBMS.sh
+++ scripts/install-RDBMS.sh
@@ -1,9 +1,14 @@
 #!/usr/bin/env bash
 
-sudo /etc/init.d/postgresql stop
-curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
-sudo apt-get update
+if [[ "$RDBMS" = PostgreSQL* ]]; then
+  sudo /etc/init.d/postgresql stop
+  curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+  sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
+  sudo apt-get update
+elif [[ "$RDBMS" = MariaDB* ]]; then
+  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
+fi
+
 if [ "$RDBMS" = "PostgreSQLv9.3" ]; then
   sudo apt-get install postgresql-9.3
 elif [ "$RDBMS" = "PostgreSQLv9.4" ]; then
@@ -18,4 +23,20 @@ elif [ "$RDBMS" = "PostgreSQLv11" ]; then
   sudo /etc/init.d/postgresql stop
   sudo apt-get remove postgresql-9.2 postgresql-client-9.2
   sudo apt-get install postgresql-11 postgresql-client-11
+elif [ "$RDBMS" = "MariaDBv10.0" ]; then
+  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'
+  sudo apt-get update
+  sudo apt-get install mariadb-server
+elif [ "$RDBMS" = "MariaDBv10.1" ]; then
+  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
+  sudo apt-get update
+  sudo apt-get install mariadb-server
+elif [ "$RDBMS" = "MariaDBv10.2" ]; then
+  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu trusty main'
+  sudo apt-get update
+  sudo apt-get install mariadb-server
+elif [ "$RDBMS" = "MariaDBv10.3" ]; then
+  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu trusty main'
+  sudo apt-get update
+  sudo apt-get install mariadb-server
 fi;
