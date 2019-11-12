61c255f51e50bfa5bad7e9b736a0c2616be3bc4b
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-11-12 15:47:42 -0300

    Update install-RDBMS.sh

--- scripts/install-RDBMS.sh
+++ scripts/install-RDBMS.sh
@@ -2,8 +2,7 @@
 
 if [[ "$RDBMS" = PostgreSQL* ]]; then
   sudo /etc/init.d/postgresql stop
-  sudo apt-get --purge remove postgresql-*
-  sudo rm -Rf /etc/postgresql /var/lib/postgresql  
+  sudo apt-get remove postgresql-*
   curl https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
   sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -cs)-pgdg main" > /etc/apt/sources.list.d/pgdg.list'
   sudo apt-get update
@@ -13,7 +12,7 @@ fi
 
 if [ "$RDBMS" = "PostgreSQLv9.4" ]; then
   sudo apt-get install postgresql-9.4 postgresql-client-9.4
-  sudo -u postgres /usr/lib/postgresql/9.4/bin/pg_ctl -D /var/lib/postgresql/9.4/main -l logfile start
+  sudo /etc/init.d/postgresql restart
 elif [ "$RDBMS" = "PostgreSQLv9.5" ]; then
   sudo apt-get install postgresql-9.5
 elif [ "$RDBMS" = "PostgreSQLv9.6" ]; then
@@ -22,7 +21,7 @@ elif [ "$RDBMS" = "PostgreSQLv10" ]; then
   sudo apt-get install postgresql-10
 elif [ "$RDBMS" = "PostgreSQLv11" ]; then
   sudo /etc/init.d/postgresql stop
-  sudo apt-get --purge remove postgresql-client-*
+  sudo apt-get remove postgresql-client-*
   sudo apt-get install postgresql-11 postgresql-client-11
 elif [ "$RDBMS" = "MariaDBv10.0" ]; then
   sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'
