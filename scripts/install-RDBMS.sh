fdb888b15fc20719e118a3f83a4b372fcf18220a
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-11-13 11:23:53 -0300

    Use docker container for MariaDB

--- scripts/install-RDBMS.sh
+++ scripts/install-RDBMS.sh
@@ -1,55 +1,19 @@
 #!/usr/bin/env bash
 
 if  [[ "$RDBMS" = MariaDB* ]]; then
-  sudo apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db
-fi
-
-if [ "$RDBMS" = "PostgreSQLv9.4" ]; then
+  readonly DOCKER_IMAGE_VERSION=echo $RDBMS | cut --complement -c -8
+  docker run -d --name mariadbtest \
+    -p 127.0.0.1:3306:3306 \
+    -e MYSQL_ROOT_PASSWORD=secret \
+    mariadb/server:$DOCKER_IMAGE_VERSION
+  docker exec mariadbtest mysqladmin -uroot -psecret create test
+elif  [[ "$RDBMS" = PostgreSQL* ]]; then
+  readonly DOCKER_IMAGE_VERSION=echo $RDBMS | cut --complement -c -11
   docker run -d -p 127.0.0.1:5432:5432 \
     -e POSTGRES_PASSWORD=secret \
     -e POSTGRES_USER=postgres \
     -e POSTGRES_DB=test \
-    postgres:9.4
-elif [ "$RDBMS" = "PostgreSQLv9.5" ]; then
-  docker run -d -p 127.0.0.1:5432:5432 \
-    -e POSTGRES_PASSWORD=secret \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_DB=test \
-    postgres:9.5
-elif [ "$RDBMS" = "PostgreSQLv9.6" ]; then
-  docker run -d -p 127.0.0.1:5432:5432 \
-    -e POSTGRES_PASSWORD=secret \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_DB=test \
-    postgres:9.6
-elif [ "$RDBMS" = "PostgreSQLv10" ]; then
-  docker run -d -p 127.0.0.1:5432:5432 \
-    -e POSTGRES_PASSWORD=secret \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_DB=test \
-    postgres:10
-elif [ "$RDBMS" = "PostgreSQLv11" ]; then
-  docker run -d -p 127.0.0.1:5432:5432 \
-    -e POSTGRES_PASSWORD=secret \
-    -e POSTGRES_USER=postgres \
-    -e POSTGRES_DB=test \
-    postgres:11
-elif [ "$RDBMS" = "MariaDBv10.0" ]; then
-  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main'
-  sudo apt-get update
-  sudo apt-get install mariadb-server
-elif [ "$RDBMS" = "MariaDBv10.1" ]; then
-  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu trusty main'
-  sudo apt-get update
-  sudo apt-get install mariadb-server
-elif [ "$RDBMS" = "MariaDBv10.2" ]; then
-  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu trusty main'
-  sudo apt-get update
-  sudo apt-get install mariadb-server
-elif [ "$RDBMS" = "MariaDBv10.3" ]; then
-  sudo add-apt-repository 'deb [arch=amd64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu trusty main'
-  sudo apt-get update
-  sudo apt-get install mariadb-server
+    postgres:$DOCKER_IMAGE_VERSION
 elif [ "$RDBMS" = "SQLite3" ]; then
   sudo apt-get install sqlite3
 fi;
--- scripts/setup-database.sh
+++ scripts/setup-database.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env bash
 
-if [[ "$RDBMS" = "MySQLv5" || "$RDBMS" = MariaDB* ]];then
-  mysql --version
-  mysql -uroot -e "CREATE DATABASE IF NOT EXISTS test;"
-  mysql -uroot -e "CREATE USER travis@localhost IDENTIFIED BY 'travis';"
-  mysql -uroot -e "GRANT ALL PRIVILEGES ON test.* TO 'travis'@'localhost';"
-  mysql -uroot -e "FLUSH PRIVILEGES;"
-elif [ "$RDBMS" = "SQLite3" ];then
+if [ "$RDBMS" = "SQLite3" ];then
   sqlite3 -version
 fi;
