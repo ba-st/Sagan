9ae70f8bfbd58d0685ac98b0b2e4343d6d002629
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-11-13 11:32:12 -0300

    Fixed script

--- scripts/install-RDBMS.sh
+++ scripts/install-RDBMS.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 if  [[ "$RDBMS" = MariaDB* ]]; then
-  readonly DOCKER_IMAGE_VERSION=echo $RDBMS | cut --complement -c -8
+  readonly DOCKER_IMAGE_VERSION=$(echo $RDBMS | cut --complement -c -8)
   docker run -d --name mariadbtest \
     -p 127.0.0.1:3306:3306 \
     -e MYSQL_ROOT_PASSWORD=secret \
     mariadb/server:$DOCKER_IMAGE_VERSION
   docker exec mariadbtest mysqladmin -uroot -psecret create test
 elif  [[ "$RDBMS" = PostgreSQL* ]]; then
-  readonly DOCKER_IMAGE_VERSION=echo $RDBMS | cut --complement -c -11
+  readonly DOCKER_IMAGE_VERSION=$(echo $RDBMS | cut --complement -c -11)
   docker run -d -p 127.0.0.1:5432:5432 \
     -e POSTGRES_PASSWORD=secret \
     -e POSTGRES_USER=postgres \
