0c5acd34b91c619612d8763663adce3f1514d878
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2019-11-13 12:41:12 -0300

    Configure MySQL docker image to allow connecting from any host

--- scripts/install-RDBMS.sh
+++ scripts/install-RDBMS.sh
@@ -10,6 +10,7 @@ elif  [[ "$RDBMS" = MySQL* ]]; then
     readonly DOCKER_IMAGE_VERSION=$(echo $RDBMS | cut --complement -c -6)
     docker run -d -p 127.0.0.1:3306:3306 \
       -e MYSQL_ROOT_PASSWORD=secret \
+      -e MYSQL_ROOT_HOST=% \
       -e MYSQL_DATABASE=test \
       mysql/mysql-server:$DOCKER_IMAGE_VERSION
 elif  [[ "$RDBMS" = PostgreSQL* ]]; then
