35a8c21118339e7e6fe25c73068aca29312305e5
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2021-12-16 12:15:53 -0300

    Use SSL for testing against PostgreSQL backend

--- scripts/setup-PostgreSQL.sh
+++ scripts/setup-PostgreSQL.sh
@@ -5,4 +5,7 @@ docker run -d -p 127.0.0.1:5432:5432 \
   -e POSTGRES_PASSWORD=secret \
   -e POSTGRES_USER=postgres \
   -e POSTGRES_DB=test \
-  postgres:"$DOCKER_IMAGE_VERSION"
+  postgres:"$DOCKER_IMAGE_VERSION" \
+  -c ssl=on \
+  -c ssl_cert_file=/etc/ssl/certs/ssl-cert-snakeoil.pem \
+  -c ssl_key_file=/etc/ssl/private/ssl-cert-snakeoil.key
