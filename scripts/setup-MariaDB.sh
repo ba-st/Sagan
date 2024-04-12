16dca7319108ea5845b048d2a0599dabff6ac9c1
Gabriel Omar Cotelli <g.cotelli@gmail.com>
2024-04-12 17:44:40 -0300

    Remove unused scripts

--- scripts/setup-MariaDB.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-
-DOCKER_IMAGE_VERSION=$(echo "$RDBMS" | cut --complement -c -8)
-readonly DOCKER_IMAGE_VERSION
-docker run -d -p 127.0.0.1:3306:3306 \
-    -e MYSQL_ROOT_PASSWORD=secret \
-    -e MYSQL_DATABASE=test \
-    mariadb/server:"$DOCKER_IMAGE_VERSION"
